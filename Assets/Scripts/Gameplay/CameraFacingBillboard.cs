02ae621ba403f692dcb0ee75a99646c886562099
Erick Subero <dracobk201@gmail.com>
2020-10-17 21:29:03 -0400

    Billboards
    - Script para crear el billboard para los enmigos.

---
+++ Assets/Scripts/Gameplay/CameraFacingBillboard.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+ 
+public class CameraFacingBillboard : MonoBehaviour
+{
+    private Camera _camera;
+ 
+    private void Start(){
+        _camera = FindObjectOfType<Camera>();
+    }
+
+    private void LateUpdate()
+    {
+        transform.LookAt(transform.position + _camera.transform.rotation * Vector3.forward, _camera.transform.rotation * Vector3.up);
+    }
+}
