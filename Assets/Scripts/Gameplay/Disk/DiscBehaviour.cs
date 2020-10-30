4bd4d70b1d47ec8f841b57b6df83185947a6d841
Erick Subero <dracobk201@gmail.com>
2020-10-30 15:44:26 -0400

    Cambio del disco en el escenario
    - Ahora el disco se frena luego de un tiempo y se muestra mejor en el mapa.

--- Assets/Scripts/Gameplay/Disk/DiscBehaviour.cs
+++ Assets/Scripts/Gameplay/Disk/DiscBehaviour.cs
@@ -3,14 +3,36 @@
 public class DiscBehaviour : MonoBehaviour
 {
     [SerializeField] private FloatReference discVelocity = null;
+    [SerializeField] private FloatReference discTimeOfLife = null;
+    [SerializeField] private GameObject discGameplaySprite = null;
+    [SerializeField] private GameObject discVerticalSprite = null;
     [SerializeField] private GameEvent enemyImpacted = null;
+    private float _actualTimeProgress;
+    private Rigidbody discRigidbody;
 
     private void OnEnable()
     {
-        TryGetComponent(out Rigidbody rigidbodyResult);
+        if (TryGetComponent(out Rigidbody rigidbodyResult))
+            discRigidbody = rigidbodyResult;
+        else
+            Debug.LogError("No Rigidbody found");
         Vector3 aux = transform.forward * discVelocity.Value;
         aux.y = 0;
         rigidbodyResult.velocity = aux;
+        _actualTimeProgress = discTimeOfLife.Value;
+        discGameplaySprite.SetActive(true);
+        discVerticalSprite.SetActive(false);
+    }
+
+    private void Update()
+    {
+        _actualTimeProgress -= Time.deltaTime;
+        if (_actualTimeProgress <= 0)
+        {
+            discRigidbody.velocity = Vector3.zero;
+            discGameplaySprite.SetActive(false);
+            discVerticalSprite.SetActive(true);
+        }
     }
 
     private void DestroyDisc()
